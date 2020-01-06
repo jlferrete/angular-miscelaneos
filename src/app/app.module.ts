abdc7802dc6825a97f401e12c84fe540ae20ec0f
JlFerrete <joseluisferrete@gmail.com>
2020-01-06 01:06:09 +0100

    Added Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { NgStyleComponent } from './components/ng-style/ng-style.component';
-import { CssComponent } from './components/css/css.component';
-import { ClasesComponent } from './components/clases/clases.component';
-import { ResaltadoDirective } from './directives/resaltado.directive';
-import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
+import { APP_ROUTING } from "./app.routes";
+
+import { AppComponent } from "./app.component";
+import { NgStyleComponent } from "./components/ng-style/ng-style.component";
+import { CssComponent } from "./components/css/css.component";
+import { ClasesComponent } from "./components/clases/clases.component";
+import { ResaltadoDirective } from "./directives/resaltado.directive";
+import { NgSwitchComponent } from "./components/ng-switch/ng-switch.component";
+import { HomeComponent } from "./components/home/home.component";
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +23,16 @@ import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
     CssComponent,
     ClasesComponent,
     ResaltadoDirective,
-    NgSwitchComponent
+    NgSwitchComponent,
+    HomeComponent,
+    UsuarioComponent,
+    UsuarioNuevoComponent,
+    UsuarioEditarComponent,
+    UsuarioDetalleComponent,
+    NavbarComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, APP_ROUTING],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
