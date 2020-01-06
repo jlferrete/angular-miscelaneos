1500b382c2a9b95f81cfa0220106c907b0d8294a
JlFerrete <joseluisferrete@gmail.com>
2020-01-06 23:06:29 +0100

    Child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,22 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { UsuarioComponent } from "./components/usuario/usuario.component";
 
+import { UsuarioNuevoComponent } from "./components/usuario/usuario-nuevo.component";
+import { UsuarioEditarComponent } from "./components/usuario/usuario-editar.component";
+import { UsuarioDetalleComponent } from "./components/usuario/usuario-detalle.component";
+
 const APP_ROUTES: Routes = [
   { path: "home", component: HomeComponent },
-  { path: "usuario", component: UsuarioComponent },
+  {
+    path: "usuario/:id",
+    component: UsuarioComponent,
+    children: [
+      { path: "nuevo", component: UsuarioNuevoComponent },
+      { path: "editar", component: UsuarioEditarComponent },
+      { path: "detalle", component: UsuarioDetalleComponent },
+      { path: "**", pathMatch: "full", redirectTo: "nuevo" }
+    ]
+  },
   { path: "**", pathMatch: "full", redirectTo: "home" }
 ];
 
